338565d962a1f2704c7a811a68f7547ebb729edc
riHilder <hilder@tutamail.com>
2026-03-13 16:44:05 +0800

    Update happy_numbers.c

--- project/happy_numbers/happy_numbers.c
+++ project/happy_numbers/happy_numbers.c
@@ -14,20 +14,28 @@ int sum_squared_digits(int n) {
     return sum;
 }
 
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
+int main() {
+    /*if (argc < 2) {
         printf("Usage: %s <number>\n", argv[0]);
         return 1;
-    }
+    }*/
 
-    int number = atoi(argv[1]);
+    //int number = atoi(argv[1]);
+    int number = 17;
 
     int slow = number;
-    int fast = sum_squared_digits(number);
+    //int fast = sum_squared_digits(number);
+   int fast = number;
 
-    while (fast != 1 && slow != fast) {
+    while (1) {
         slow = sum_squared_digits(slow);
+        printf("%d     ", slow);
+
         fast = sum_squared_digits(sum_squared_digits(fast));
+        printf("%d\n", fast);
+
+        if (slow == fast)
+            printf("\n");
     }
 
     if (fast == 1) {
