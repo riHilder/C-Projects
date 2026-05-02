691125560fe567f307783704c48ca08532b36bc2
riHilder <hilder@tutamail.com>
2026-05-02 14:25:44 +0800

    Update tictactoe.c

--- project/tictactoe/tictactoe.c
+++ project/tictactoe/tictactoe.c
@@ -63,15 +63,19 @@ int check_win(char grid[3][3], char player)
         if (check_row(grid, player, i))
             return 1;
     }
+
     for (int i = 0; i < 3; i++)
     {
         if (check_col(grid, player, i))
             return 1;
     }
+
     if (check_diagonal(grid, player))
         return 1;
+
     if (check_anti_diagonal(grid, player))
         return 1;
+
     return 0;
 }
 
@@ -90,65 +94,87 @@ int full_grid(char grid[3][3])
     return 1;
 }
 
-int minimax(char grid[3][3], char player)
+int MINIMAX(char grid[3][3], char player)
 {
-    if (check_win(grid, 'X'))
-        return -10;
     if (check_win(grid, 'O'))
         return 10;
+    if (check_win(grid, 'X'))
+        return -10;
     if (full_grid(grid))
         return 0;
 
+
     if (player == 'O') {
-        int best_val = -INT_MAX;
+        int max_val = -INT_MAX;
+        int best_move;
+
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 if (grid[i][j] == ' ') {
                     grid[i][j] = 'O';
-                    int val = minimax(grid, 'X');
+                    int val = MINIMAX(grid, 'X');
+                    printf("(O) val: %d(%d)\n", val, i * 3 + j + 1);
+
                     grid[i][j] = ' ';
-                    if (val > best_val) 
-                        best_val = val;
+                    if (val > max_val)
+                        max_val = val;
+
+                        int best_move = i * 3 + j + 1;
+                    }
                 }
             }
-        }
-        return best_val;
+
+        printf("(O) max_val: %d(%d)\n", max_val, best_move );
+        return max_val;
+
     } else {
-        int best_val = INT_MAX;
+        int min_val = INT_MAX;
+        int best_move;
+
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 if (grid[i][j] == ' ') {
                     grid[i][j] = 'X';
-                    int val = minimax(grid, 'O');
+                    int val = MINIMAX(grid, 'O');
+                    printf("(X) val: %d(%d)\n", val,  i * 3 + j + 1);
+
                     grid[i][j] = ' ';
-                    if (val < best_val) 
-                        best_val = val;
+                    if (val < min_val)
+                        min_val = val;
+                        best_move = i * 3 + j + 1;
                 }
             }
         }
-        return best_val;
+
+        printf("(X) min_val: %d(%d)\n", min_val, best_move);
+        return min_val;
     }
 }
 
-int findBestMove(char grid[3][3]) {
+int findInvincibleMove(char grid[3][3]) {
     int best_val = -INT_MAX;
-    int best_move = -1;
+    int invincible_move;
 
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             if (grid[i][j] == ' ') {
                 grid[i][j] = 'O';
-                int move_val = minimax(grid, 'X');
+                int move_val = MINIMAX(grid, 'X');
                 grid[i][j] = ' ';
+                printf("move_val:%d(%d)\n", move_val, i * 3 + j + 1);
+
                 if (move_val > best_val) {
                     best_val = move_val;
-                    best_move = i * 3 + j;
+                    invincible_move = i * 3 + j + 1;
                 }
             }
         }
     }
-    return best_move;
+    printf("invincible_move:%d(%d)\n", best_val, invincible_move);
+    return invincible_move;
 }
+
+
 
 int main()
 {
@@ -159,6 +185,7 @@ int main()
     printf("Welcome to tictactoe game!\nYou are X, computer is O\n");
     printf("Choose a position from 1 to 9\n");
     print(grid);
+
 
     while (1)
     {
@@ -196,9 +223,10 @@ int main()
             break;
         }
 
-        int pc_move = findBestMove(grid);
-        row = pc_move / 3;
-        col = pc_move % 3;
+
+        int pc_move = findInvincibleMove(grid);
+        row = (pc_move - 1) / 3;
+        col = (pc_move - 1) % 3;
         grid[row][col] = 'O';
 
         print(grid);
